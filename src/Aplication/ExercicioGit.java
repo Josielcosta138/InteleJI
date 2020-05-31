838e5b630bb757f65fc8a8d697130fc8a3c0d567
Josielcosta138 <cjosiel2@gmail.com>
2020-05-31 13:11:59 -0300

    Alterado edições para Versao 2

--- src/Aplication/ExercicioGit.java
+++ src/Aplication/ExercicioGit.java
@@ -2,7 +2,11 @@ package Aplication;
 
 public class ExercicioGit {
     public static void main(String args[]){
-        System.out.println("Primeiro exercicio GitBash");
+        System.out.println(" 1º exercicio Branch -Master-");
+        System.out.println(" <------------------------------>");
+            System.out.println(" 2º exercicio Branch -Versão 2-")
+        ;
 
+            
     }
 }
